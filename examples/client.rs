1ecba7af7232c9507564b08da54e9a2b043275f3
lmmqxyx <1368470804@qq.com>
2024-06-24 12:06:08 +0800

    complete basic client structure

--- examples/client.rs
+++ examples/client.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("hello world");
+use socks5_scratch::Result;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    env_logger::init();
+
+    spawn_socks_client().await
+}
+
+async fn spawn_socks_client() -> Result<()> {
+    todo!()
 }
---
+++ src/lib.rs
@@ -0,0 +1,8 @@
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum SocksError {
+
+}
+
+pub type Result<T, E = SocksError> = core::result::Result<T, E>;
