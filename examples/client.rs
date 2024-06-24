66bd5215b667511b656a87efa99a1e91140bdd6b
lmmqxyx <1368470804@qq.com>
2024-06-24 12:16:01 +0800

    add client conf

--- examples/client.rs
+++ examples/client.rs
@@ -1,5 +1,25 @@
 use socks5_scratch::Result;
 
+#[derive(Debug)]
+struct Opt {
+    /// Socks5 server address + port. eg. `127.0.0.1:1080`
+    pub socks_server: String,
+
+    /// Target address server (not the socks server)
+    pub target_addr: String,
+
+    /// Target port server (not the socks server)
+    pub target_port: u16,
+
+    /// 可以不配置
+    pub username: Option<String>,
+    /// 可以不配置
+    pub password: Option<String>,
+
+    /// Don't perform the auth handshake, send directly the command request
+    pub skip_auth: bool,
+}
+
 #[tokio::main]
 async fn main() -> Result<()> {
     env_logger::init();
@@ -8,5 +28,14 @@ async fn main() -> Result<()> {
 }
 
 async fn spawn_socks_client() -> Result<()> {
+    let opt = Opt {
+        password: None,
+        username: None,
+        skip_auth: true,
+        socks_server: "127.0.0.1:1337".to_owned(),
+        // 可以自行替换
+        target_addr: "www.github.com".to_owned(),
+        target_port: 443,
+    };
     todo!()
 }
